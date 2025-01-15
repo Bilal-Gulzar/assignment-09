c2c668e3c1ce8267b3fb2071d1d064fdd1e0865f
bilal gul <bilalgul415@gmail.com>
2025-01-15 16:17:46 +0500

    add loading file

--- src/app/client-side-code/[slug]/page.tsx
+++ src/app/client-side-code/[slug]/page.tsx
@@ -31,20 +31,6 @@ return (
     <div>
       <section className="text-gray-600 body-font overflow-hidden">
         <div className="container px-5 py-24 mx-auto">
-          <Suspense
-            fallback={
-              <div className="lg:w-4/5 gap-10 mx-auto flex flex-wrap">
-                <Skeleton className="bg[#f5f5f5]  lg:w-1/2 w-full relative h-[370px]" />
-                <div className=" flex-grow flex lg:mt-5 flex-col gap-5">
-                  <Skeleton className="w-[28%] h-9" />
-                  <Skeleton className="w-[90%] lg:w-[80%]  h-7" />
-                  <Skeleton className="w-[80%] lg:w-[70%] h-7" />
-                  <Skeleton className=" w-[90%] lg:w-[80%] h-28 " />
-                  <Skeleton className="w-[25%] h-10 " />
-                </div>
-              </div>
-            }
-          >
             <div className="lg:w-4/5 mx-auto flex flex-wrap">
               {/* <Image
               alt="ecommerce"
@@ -228,7 +214,6 @@ return (
                 </div>
               </div>
             </div>
-          </Suspense>
         </div>
       </section>
     </div>
